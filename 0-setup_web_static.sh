cbb8465a2b15804e2157af2e742e8379023228e9
Tshepiso <tlhongtshepiso2@gmail.com>
2024-04-04 11:30:15 +0200

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,14 @@ sudo apt-get -y install nginx
 #Create the folders
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-#Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
+
+'''#Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
 echo "<html>
   <head>
   </head>
@@ -32,5 +39,5 @@ config="location /hbnb_static/ {
 sudo sed -i "36i $config" /etc/nginx/sites-available/default
 #Use alias inside your Nginx configuration
 sudo service nginx start
-exit 0
+exit 0'''
 
