e33abb12186f490d3648a16016de7341185a19b5
Tshepiso <tlhongtshepiso2@gmail.com>
2024-07-10 13:42:50 +0200

    made changes in this repo to be able to redo my work

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,11 @@
-#!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+#!/usr/bin/env bash 
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginc HTTP'
+#installing nginx
+sudo apt update
+sudo apt install ngnix 
 
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-" | sudo tee /data/web_static/releases/test/index.html
+#creating required file and folder
+mkdir -p data/web_static/releases/test/ data/web_static/shared/
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+touch data/web_static/releases/test/index.html
 
-sudo chown -hR ubuntu:ubuntu /data/
-
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-sudo service nginx start
