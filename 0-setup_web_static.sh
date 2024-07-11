61a0ba226e10524de6a1c92a46162e04dde4d511
Tshepiso <tlhongtshepiso2@gmail.com>
2024-07-11 12:30:57 +0200

    updating the 0-setup_web_static.sh file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash 
 
-#installing nginx
-sudo apt update
-sudo apt install ngnix 
+# installing nginx
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
 
-#creating required file and folder
+
+# creating required file and folder
 mkdir -p data/web_static/releases/test/ data/web_static/shared/
 
 sudo echo "
@@ -16,10 +18,13 @@ sudo echo "
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-#creating a symbolic link
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+# creating a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-#changing ownership
-sudo chown -R ubuntu:ubuntu /data
+# changing ownership
+sudo chown -hR ubuntu:ubuntu /data
 sudo ls -l /data # verifying change
-
+# configuring nginx
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# starting nginx
+sudo service nginx start
