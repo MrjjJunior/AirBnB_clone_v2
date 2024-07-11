039fa7a43430ec8ccf6baeebcb7aba070d470a9c
Tshepiso <tlhongtshepiso2@gmail.com>
2024-07-12 00:01:41 +0200

    editing setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash 
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
+# Install Nginx if it not already installed
+if ! dpkg -l | grep -q nginx; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
 sudo ufw allow 'Nginx HTTP'
-mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 sudo echo "
 <html>
   <head>
@@ -13,8 +17,13 @@ sudo echo "
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -hR ubuntu:ubuntu /data
+
 sudo ls -l /data
+
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
 sudo service nginx start
