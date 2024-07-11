f0027105f072181b590b48698ab7e8013212b05d
Tshepiso <tlhongtshepiso2@gmail.com>
2024-07-11 12:46:34 +0200

    setting up

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash 
 
-# installing nginx
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-
-
-# creating required file and folder
+sudo ufw allow 'Nginc HTTP'
 mkdir -p data/web_static/releases/test/ data/web_static/shared/
-
 sudo echo "
 <html>
   <head>
@@ -17,14 +13,8 @@ sudo echo "
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-
-# creating a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# changing ownership
 sudo chown -hR ubuntu:ubuntu /data
-sudo ls -l /data # verifying change
-# configuring nginx
+sudo ls -l /data
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-# starting nginx
 sudo service nginx start
