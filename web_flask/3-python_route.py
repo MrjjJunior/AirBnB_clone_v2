31caae54072ff2a26df7755aa8801f1a2bad6e8c
Tshepiso <tlhongtshepiso2@gmail.com>
2024-07-29 00:22:43 +0200

    python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,9 +21,9 @@ def c_is_fun(text):
     ''' '''
     return "C " + text.replace('_', ' ')
 
-
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text):
+def python_is_cool(text='is cool'):
     ''' '''
     return "Python " + text.replace('_', ' ')
 
